077d57d39c2712e33747abe6588e3b1aa73ddf2c
TAMILSELVANPOOKZE <tamilselvan28032000@gmail.com>
2026-02-24 19:11:15 +0530

    Updated start script to use server.js

--- app.js
+++ app.js
@@ -35,7 +35,4 @@ app.get('/health', (req, res) => {
   res.json({ status: 'ok' });
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`App running on port ${PORT}`));
-
 module.exports = app;
